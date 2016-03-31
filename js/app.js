1aae5747f541024ece80f5cc693f777cc7edc876
slkcoin <xcoin@me.com>
2016-03-31 11:31:23 -0700

    Primary Update #1
    Still working on major features. Player / Enemy collision is last part

--- js/app.js
+++ js/app.js
@@ -1,34 +1,109 @@
+var random = function() {
+  return Math.floor(Math.random() * 500) + 100;
+}
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(location) {
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  this.sprite = 'images/enemy-bug.png';
+  this.x = 202;
+  this.y = location;
+  this.Row = Math.floor(location / 83);
+  console.log(this.Row);
+  this.Col = Math.floor(this.x / 101);
+  console.log(this.Col);
+  this.speed = 100;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+  if (this.x < rightBotBoundary ) {
+    this.x += 50 * dt;
+    this.Col = Math.floor(this.x / 101);
+    console.log(this.Col);
+  }
+  else{
+    this.x = -101;
+    this.Col = 0;
+  }
+  if (player.Row == this.Row && player.Col == this.Col) {
+    player.x = 200;
+    player.y = 400;
+    player.Col = 1;
+    player.Row = 4;
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(){
+  this.sprite = 'images/char-boy.png';
+  this.x = 200;
+  this.y = 400;
+  this.Row = Math.floor(this.y / 83);
+  this.Col = Math.floor(this.x / 101);
+
+};
+
+Player.prototype.update = function() {
+
+  if (this.x > rightBotBoundary) {
+    this.x = rightBotBoundary;
+  }
+  if (this.x < leftTopBoundary) {
+    this.x = leftTopBoundary;
+  }
+  if (this.y > rightBotBoundary) {
+    this.y = rightBotBoundary;
+  }
+  if (this.y < leftTopBoundary) {
+    this.x = 200;
+    this.y = 400;
+    this.Col = 1;
+    this.Row = 4;
+  }
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(input) {
+  if (input == 'up') {
+    this.y -= 83;
+  }
+  if (input == 'down') {
+    this.y += 83;
+  }
+  if (input == 'left') {
+    this.x -= 101;
+  }
+  if (input == 'right') {
+    this.x += 101;
+  }
+};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var row = 55;
+
+var rightBotBoundary = 400;
+var leftTopBoundary = 0;
+
+var allEnemies = [new Enemy(row)];
+
+var player = new Player;
 
 
 
